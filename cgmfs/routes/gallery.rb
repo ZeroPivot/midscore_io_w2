a4da2c6f9a7fad849439041168e90713540b542f
AxleShep <eigenfruit@gmail.com>
2024-02-27 15:31:12 +0000

    tinymc update, gallery.rb update

--- cgmfs/routes/gallery.rb
+++ cgmfs/routes/gallery.rb
@@ -9,6 +9,7 @@ class CGMFS
   plugin :json
 
   hash_branch('gallery') do |r|
+    r.hash_branches
     r.is do
       r.get do
         # Retrieve all galleries
@@ -20,34 +21,6 @@ class CGMFS
         # Create a new gallery
         gallery_id = DB[:galleries].insert(title: r.params['title'])
         { gallery_id: gallery_id }
-      end
-    end
-
-    r.on 'folder', String, Integer do |username, folder_id|
-      r.get do
-        # Retrieve a specific folder
-        folder = DB[:folders].first(id: folder_id)
-        { folder: folder }
-      end
-
-      r.post do
-        # Update a specific folder
-        updated_folder = DB[:folders].where(id: folder_id).update(title: r.params['title'])
-        { folder_id: folder_id, message: 'Folder updated successfully' }
-      end
-    end
-
-    r.on 'folder', String, Integer, 'edit' do |username, folder_id|
-      r.get do
-        # Retrieve the edit page for a specific folder
-        folder = DB[:folders].first(id: folder_id)
-        { folder: folder }
-      end
-
-      r.post do
-        # Update a specific folder from the edit page
-        updated_folder = DB[:folders].where(id: folder_id).update(title: r.params['title'])
-        { folder_id: folder_id, message: 'Folder updated successfully' }
       end
     end
 
@@ -66,30 +39,6 @@ class CGMFS
       end
     end
 
-
-    r.on 'signup' do
-      r.post do
-        # Handle user signup
-        # Create a new user record in the database
-        { message: 'User signed up successfully' }
-      end
-    end
-
-    r.on 'login' do
-      r.post do
-        # Handle user login
-        # Authenticate user credentials
-        { message: 'User logged in successfully' }
-      end
-    end
-
-    r.on 'logout' do
-      r.post do
-        # Handle user logout
-        # Clear user session
-        { message: 'User logged out successfully' }
-      end
-    end
   end
 end
 

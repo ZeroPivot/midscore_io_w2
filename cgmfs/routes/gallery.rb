99a9461d1d08d892e4026994fecd22b8b27f9df9
AxleShep <eigenfruit@gmail.com>
2024-03-01 01:05:33 +0000

    wip

--- cgmfs/routes/gallery.rb
+++ cgmfs/routes/gallery.rb
@@ -1,4 +1,4 @@
-# rubocop:disable Metrics/BlockLength
+# rubocop:disable Metrics/BlockLength, Layout/LineLength, Metrics/ClassLength
 class CGMFS
   def user_failcheck(username, r)
     return if @@line_db.databases.include?(username)
@@ -45,17 +45,13 @@ class CGMFS
       # get user session in roda
       @user = session['user']
       logged_in?(r, @user)
+      user_failcheck(@user, r)
 
       r.get do
-        if @user
-          view('blog/gallery/new', engine: 'html.erb', layout: 'layout.html')
-        else
-          "<html><body>You are not logged in. Please log in to upload an image. <a href='#{domain_name(r)}/blog/login'>Login</a></html></body>. Session_Info: #{@user}"
-        end
+        view('blog/gallery/new', engine: 'html.erb', layout: 'layout.html')
       end
 
       r.post do
-        logged_in?(r, @user)
         # Code to upload an image to the gallery, with an option to introduce the upload location and retrieve via URL
         # 1. Upload the image to the server
         # 2. Save the image to the database
@@ -143,6 +139,79 @@ class CGMFS
         view('blog/gallery/view_user_gallery_image_id', engine: 'html.erb', layout: 'layout.html')
       end
     end
+    # /gallery/edit/user/id/ID
+    r.is 'edit', String, 'id', Integer do |user, id| # edit the gallery list
+      user_failcheck(user, r)
+      logged_in?(r, user)
+      r.get do
+        @user = user
+        @gallery = @@line_db[@user].pad['gallery_database', 'gallery_table']
+        @id = id
+
+        @image = @gallery.get(@id)
+        @title = @image['title']
+        @description = @image['description']
+        @tags = @image['tags']
+
+        view('blog/gallery/edit_user_gallery_image_id', engine: 'html.erb', layout: 'layout.html')
+      end
+      r.post do
+        @user = user
+        @gallery = @@line_db[@user].pad['gallery_database', 'gallery_table']
+        @id = id
+        @title = r.params['title']
+        @description = r.params['description']
+        @tags = r.params['tags']
+        @image = @gallery.set(@id) do |hash|
+          hash['title'] = @title
+          hash['description'] = @description
+          hash['tags'] = @tags
+          hash['date'] = TZInfo::Timezone.get('America/Los_Angeles').utc_to_local(Time.now).to_s
+        end
+
+
+        # get the image temp file parameters through roda:
+        uploadable = false
+        uploaded_filehandle = r.params['file']
+        original_to_new_filename = "#{Time.now.to_f}_#{uploaded_filehandle[:filename]}"
+        file_contents = uploaded_filehandle[:tempfile].read
+        file_size = file_contents.size
+        file_extension = File.extname(uploaded_filehandle[:filename])
+        # list all possible file types in File.extname:
+        # .jpg, .jpeg, .png, .gif, .bmp, .zip, .tar, .gz, .rar, .7z, .mp3, .wav, .flac, .ogg, .mp4, .avi, .mkv, .mov, .wmv, .flv, .webm, .pdf, .doc, .docx, .xls, .xlsx, .ppt, .pptx, .txt, .rtf, .html, .htm, .xml, .json, .csv, .tsv, .md, .markdown, .rb, .py, .js, .css, .scss, .sass, .less, .php, .java, .c, .cpp, .h, .hpp, .cs, .go, .swift, .kt, .kts, .rs, .pl, .sh, .bat, .exe, .dll, .so, .dylib, .app, .apk, .ipa, .deb, .rpm, .msi, .dmg, .iso, .img, .bin, .cue, .mdf, .mds, .nrg, .vcd, .toast, .dmg, .toast, .vcd, .nrg, .mds, .mdf, .cue, .bin, .img, .iso, .rpm, .msi, .deb, .ipa, .apk, .app, .dylib, .so, .dll, .exe, .bat, .sh, .pl, .rs, .kts, .kt, .swift, .go, .cs, .hpp, .h, .cpp, .c, .java, .php, .less, .sass, .scss, .css, .js, .py, .rb, .markdown, .md, .tsv, .csv, .json, .xml, .htm, .html, .rtf, .txt, .pptx, .ppt, .xlsx, .xls, .docx, .doc, .pdf, .webm, .flv, .wmv, .mov, .mkv, .avi, .mp4, .ogg, .flac, .wav, .mp3, .7z, .rar, .gz, .
+        #
+        if ['.jpg', '.jpeg', '.png', '.gif', '.bmp'].include?(file_extension) # add .zip later, et al.
+          uploadable = true
+          FileUtils.mkdir_p("public/gallery/#{@user}")
+          File.open("public/gallery/#{@user}/#{original_to_new_filename}", 'w') { |file| file.write(file_contents) }
+        else
+          uploadable = false
+        end
+
+        if uploadable
+          @@line_db[@user].pad['gallery_database', 'gallery_table'].add do |hash|
+            hash['file'] = original_to_new_filename
+            hash['views'] = @image['views']
+            hash['title'] = @image
+            hash['description'] = @image['description']
+            hash['downloads'] = 0
+            hash['shares'] = 0
+            hash['comments'] = 0
+            hash['likes'] = 0
+            hash['tags'] = @image['tags']
+            hash['size'] = file_size
+            hash['extension'] = file_extension
+            hash['date'] = TZInfo::Timezone.get('America/Los_Angeles').utc_to_local(Time.now).to_s
+          end
+        end
+
+
+
+
+        @gallery.save_partition_by_id_to_file!(@id)
+        r.redirect "#{domain_name(r)}/gallery/view/#{@user}"
+      end
+    end
   end
 end
-# rubocop:enable Metrics/BlockLength
+# rubocop:enable Metrics/BlockLength, Layout/LineLength, Metrics/ClassLength

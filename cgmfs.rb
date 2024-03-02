6c23355cf2f43cf1ebd35cdd3d99706c22fe556e
AxleShep <eigenfruit@gmail.com>
2024-03-02 16:22:33 +0000

    v3.3.3.0 -- " gusty stink"

--- cgmfs.rb
+++ cgmfs.rb
@@ -46,7 +46,7 @@ SERVER_IP = SERVER_MAIN_DOMAIN_NAME
 SERVER_IP_LOCAL = 'localhost'
 DOMAIN_NAME = "https://#{SERVER_MAIN_DOMAIN_NAME}"
 
-$dog_blog_version = "v3.3.2.9" # used in layout.html.erb
+$dog_blog_version = "v3.3.3.0 - \"Gusty Stink\"" # used in layout.html.erb
 
 DO_TELEGRAM_LOGGING = true # telegram logging
 

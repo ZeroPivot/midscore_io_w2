3d913f5a8422acfb4c463d3ee3297af90900395b
AxleShep <eigenfruit@gmail.com>
2024-03-04 16:36:27 +0000

    pre1

--- cgmfs.rb
+++ cgmfs.rb
@@ -48,7 +48,7 @@ SERVER_IP = SERVER_MAIN_DOMAIN_NAME
 SERVER_IP_LOCAL = 'localhost'
 DOMAIN_NAME = "https://#{SERVER_MAIN_DOMAIN_NAME}"
 
-$dog_blog_version = "v3.3.4.1pre0 - Codename: \"The Stimky Whiff\"" # used in layout.html.erb
+$dog_blog_version = "v3.3.4.1pre1 - Codename: \"The Stimky Whiff\"" # used in layout.html.erb
 
 DO_TELEGRAM_LOGGING = true # telegram logging
 
@@ -76,7 +76,7 @@ class CGMFS < Roda
   plugin :assets, css: ['style.css'], js: ['script.js']
   plugin :json
   plugin :json_parser
-  
+
   # plugin :csrf, :raise=>true, :skip=>['POST:/api/screens']
   # <%= assets(:css) %>
   # <%= assets(:js) %>

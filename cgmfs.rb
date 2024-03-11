c51a30e92147adc7253d1aa16a5259cd9ca62ae7
AxleShep <eigenfruit@gmail.com>
2024-03-11 19:52:07 +0000

    v3.3.6.1a - dont compact gallery on listing

--- cgmfs.rb
+++ cgmfs.rb
@@ -52,7 +52,7 @@ SERVER_IP = SERVER_MAIN_DOMAIN_NAME
 SERVER_IP_LOCAL = 'localhost'
 DOMAIN_NAME = "https://#{SERVER_MAIN_DOMAIN_NAME}"
 
-$dog_blog_version = "v3.3.6.0 - Codename: \"The Stimky Sniffa\"" # used in layout.html.erb
+$dog_blog_version = "v3.3.6.1a - Codename: \"The Stimky Sniffa\"" # used in layout.html.erb
 
 DO_TELEGRAM_LOGGING = true # telegram logging
 

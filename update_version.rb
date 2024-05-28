51b57d7d20cfe853f39ebc5f921ba47ca2d57824
AxleShep <eigenfruit@gmail.com>
2024-05-28 05:11:21 +0000

    update_version.rb update to 0.0.0.0 versioning scheme

--- update_version.rb
+++ update_version.rb
@@ -1,27 +1,36 @@
 require 'tzinfo'
 
+
 # Get the current version from the file
-current_version = File.read('version.txt').to_f
+current_version = File.read('version.txt').strip
 
-# Increment the version by 0.1
-new_version = (current_version + 0.1).round(1).to_s
+version_parts = current_version.split('.').map(&:to_i)
 
-# Add the decimal point if necessary
-new_version += '.0' unless new_version.include?('.')
+# Start from the rightmost part and move left
+(version_parts.length - 1).downto(0) do |i|
+  if version_parts[i] < 9
+    # Increment the part and reset all parts to the right of it to 0
+    version_parts[i] += 1
+    ((i + 1)...version_parts.length).each { |j| version_parts[j] = 0 }
+    break
+  end
+end
+
+new_version = version_parts.join('.')
 
 # Write the new version to the file
 File.open('version.txt', 'w') do |file|
   file.puts new_version
 end
 
+
 # Get the current timestamp
 puts "Current version: #{current_version}"
 puts "New version: #{new_version}"
 
-
 # Get the current timestamp in Pacific Time
 timezone = TZInfo::Timezone.get('America/Los_Angeles')
-timestamp = timezone.now.strftime("%Y-%m-%d %H:%M:%S")
+timestamp = timezone.now.strftime('%Y-%m-%d %H:%M:%S')
 puts "Timestamp: #{timestamp} Pacific Time"
 
 # Write the new version and timestamp to the file

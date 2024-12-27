ae2763c7a8c8b098f7a75e31d946c926e4c6e37a
AylonArlon <eigenfruit@gmail.com>
2024-12-27 03:08:04 -0800

    v1.2 push continue

--- lib/loggers/README.md
+++
@@ -1,7 +0,0 @@
-Currently working on primarily on the following projects:
-
-`telegram_chat_openai.rb`
-
-Everything else helps but aren't as relevant to the project.
-
-2023-04-14 - 11:24PM
--- update_hud_restart.rb
+++
@@ -1,6 +0,0 @@
-
-puts 'Updating HUD...'
-puts `ruby update_version.rb`
-`kill -9 $(ps aux | grep puma | awk '{print $2}')`
-exec('puma -C config/puma-nginx-production-hud.rb &')
-puts 'Puma server restarted.'

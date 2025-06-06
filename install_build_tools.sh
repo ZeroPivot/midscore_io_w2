86fa8efac93261d01888d3f9a6bb27d4d5fdcb0f
InfiniVaeria <aritywolf@gmail.com>
2025-06-06 14:22:19 -0700

    changes

---
+++ install_build_tools.sh
@@ -0,0 +1,5 @@
+sudo apt install build-essential
+sudo apt install cmake
+sudo apt install clang
+sudo apt install autoconf automake libffi-dev zlib1g-dev libssl-dev
+sudo apt-get install libyaml-dev

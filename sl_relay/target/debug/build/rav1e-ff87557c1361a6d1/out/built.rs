ff3ddb09d5b3a4002bbe61535c60a044f67a17ff
The Field Testers <midscore.io@gmail.com>
2025-12-28 07:57:04 +0000

    update

--- sl_relay/target/debug/build/rav1e-ff87557c1361a6d1/out/built.rs
+++ sl_relay/target/debug/build/rav1e-ff87557c1361a6d1/out/built.rs
@@ -75,10 +75,10 @@ pub static FEATURES_LOWERCASE: [&str; 1] = ["threading"];
 pub static FEATURES_LOWERCASE_STR: &str = "threading";
 #[doc=r#"The output of `rustc -V`"#]
 #[allow(dead_code)]
-pub static RUSTC_VERSION: &str = "rustc 1.94.0-nightly (21ff67df1 2025-12-15)";
+pub static RUSTC_VERSION: &str = "rustc 1.94.0-nightly (7b5cde737 2025-12-27)";
 #[doc=r#"The output of `rustdoc -V`; empty string if `rustdoc -V` failed to execute"#]
 #[allow(dead_code)]
-pub static RUSTDOC_VERSION: &str = "rustdoc 1.94.0-nightly (21ff67df1 2025-12-15)";
+pub static RUSTDOC_VERSION: &str = "rustdoc 1.94.0-nightly (7b5cde737 2025-12-27)";
 #[doc=r#"The target architecture, given by `CARGO_CFG_TARGET_ARCH`."#]
 #[allow(dead_code)]
 pub static CFG_TARGET_ARCH: &str = "x86_64";

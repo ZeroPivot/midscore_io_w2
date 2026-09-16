0dfa28ed5ae1573131de7b8e17dbb527a096ea9d
ZeroPivot <aritywolf@gmail.com>
2026-09-16 17:15:40 +0000

    update

---
+++ wasm-client/target/release/build/midscore-wasm-client/0b7a168ce286b0c2/out/midscore_wasm_client.d
@@ -0,0 +1,5 @@
+/root/midscore_io/wasm-client/target/release/build/midscore-wasm-client/0b7a168ce286b0c2/out/midscore_wasm_client.d: src/lib.rs
+
+/root/midscore_io/wasm-client/target/release/build/midscore-wasm-client/0b7a168ce286b0c2/out/libmidscore_wasm_client.so: src/lib.rs
+
+src/lib.rs:
---
+++ wasm-client/target/release/libmidscore_wasm_client.d
@@ -0,0 +1,1 @@
+/root/midscore_io/wasm-client/target/release/libmidscore_wasm_client.so: /root/midscore_io/wasm-client/src/lib.rs
